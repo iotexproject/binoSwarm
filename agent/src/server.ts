0b2634ad6f33dd8d926f057bbff85c475552a967
Nikita Ruban <nikitkaruban@gmail.com>
2025-03-25 10:53:35 +0000

    refactor: move port check to a separate file

---
+++ agent/src/server.ts
@@ -0,0 +1,20 @@
+import net from "net";
+
+export const checkPortAvailable = (port: number): Promise<boolean> => {
+    return new Promise((resolve) => {
+        const server = net.createServer();
+
+        server.once("error", (err: NodeJS.ErrnoException) => {
+            if (err.code === "EADDRINUSE") {
+                resolve(false);
+            }
+        });
+
+        server.once("listening", () => {
+            server.close();
+            resolve(true);
+        });
+
+        server.listen(port);
+    });
+};
