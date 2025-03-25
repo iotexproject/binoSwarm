a3b2eaf5da984903c6914801b859e19abcb8872f
Nikita Ruban <nikitkaruban@gmail.com>
2025-03-25 13:14:01 +0000

    refactor: consolidate agent startup logic into index.ts

--- agent/src/agent.ts
+++ agent/src/agent.ts
@@ -8,27 +8,13 @@ import {
     IDatabaseCacheAdapter,
 } from "@elizaos/core";
 
-import { parseArgsAndLoadCharacters } from "./characterLoader";
-import { initAndStartDirectClient } from "./server";
 import { getTokenForProvider } from "./modelProviders";
 import { initializeDatabase } from "./db";
 import { initializeCache } from "./cache";
 import { initializeClients } from "./clients";
 import { buildPlugins } from "./plugins";
 
-export const startAgents = async () => {
-    const characters = await parseArgsAndLoadCharacters();
-    const runtimes: AgentRuntime[] = [];
-
-    await initializeStartupAgents(characters, runtimes);
-    await initAndStartDirectClient(runtimes);
-
-    elizaLogger.log(
-        "Run `pnpm start:client` to start the client and visit the outputted URL (http://localhost:5173) to chat with your agents. When running multiple agents, use client with different port `SERVER_PORT=3001 pnpm start:client`"
-    );
-};
-
-async function initializeStartupAgents(
+export async function initializeStartupAgents(
     characters: Character[],
     runtimes: AgentRuntime[]
 ) {
