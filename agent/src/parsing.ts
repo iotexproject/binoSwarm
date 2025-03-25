7d8d0940401061f90b30978782add08e4d62bb17
Nikita Ruban <nikitkaruban@gmail.com>
2025-03-25 07:49:07 +0000

    feat: move character argument parsing to separate file

---
+++ agent/src/parsing.ts
@@ -0,0 +1,24 @@
+import yargs from "yargs";
+import { elizaLogger } from "@elizaos/core";
+
+export function parseArguments(): {
+    character?: string;
+    characters?: string;
+} {
+    try {
+        return yargs(process.argv.slice(3))
+            .option("character", {
+                type: "string",
+                description: "Path to the character JSON file",
+            })
+            .option("characters", {
+                type: "string",
+                description:
+                    "Comma separated list of paths to character JSON files",
+            })
+            .parseSync();
+    } catch (error) {
+        elizaLogger.error("Error parsing arguments:", error);
+        return {};
+    }
+}
