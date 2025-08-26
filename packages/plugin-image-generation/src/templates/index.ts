131d763bc51e36f0a3f34759eebf66bd045a5bbb
Nikita Ruban <nikitkaruban@gmail.com>
2025-08-26 12:53:35 +0000

    refactor(templates): remove unused current message placeholder
    - Eliminated the `<current_message>` placeholder from the template, as it was no longer necessary, improving code clarity and reducing clutter.

--- packages/plugin-image-generation/src/templates/index.ts
+++ packages/plugin-image-generation/src/templates/index.ts
@@ -37,10 +37,6 @@ Now analyze the following content and create a prompt:
 {{topics}}
 </topics_of_interest>
 
-<current_message>
-{{currentMessage}}
-</current_message>
-
 </character_description>
 `;
 
