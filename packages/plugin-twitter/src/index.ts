219df8cc19a22d91241c889fb8c5a3bbcdf4a0e9
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2026-01-30 13:30:56 +0000

    feat: init plugin twitter

---
+++ packages/plugin-twitter/src/index.ts
@@ -0,0 +1,16 @@
+import type { Plugin } from "@elizaos/core";
+
+export const twitterPlugin: Plugin = {
+    name: "twitter",
+    description: "Twitter plugin",
+    providers: [],
+    evaluators: [
+        // Add evaluators here
+    ],
+    services: [
+        // Add services here
+    ],
+    actions: [],
+};
+
+export default twitterPlugin;
---
+++ packages/plugin-twitter/tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    outDir: "dist",
+    sourcemap: true,
+    clean: true,
+    format: ["esm"],
+    external: [
+        "dotenv",
+        "fs",
+        "path",
+        "@reflink/reflink",
+        "https",
+        "http",
+        "agentkeepalive",
+        "events",
+        "node-cache",
+        "axios",
+    ],
+});
